6f6f74215422a348aee3e04f7ace2743d7e9478e
ayeolakenny <ayeolakenny@gmail.com>
2023-02-14 08:50:18 +0100

    cors issue

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,11 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const PORT = process.env.PORT || 4000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: true,
+        methods: "GET,HEAD,PUT,PATCH,DELETE,OPTIONS",
+        credentials: true,
+    });
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(PORT);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import 'dotenv/config';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import "dotenv/config";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,DELETE,OPTIONS",
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
