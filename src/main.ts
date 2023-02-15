cd7fc7d9e147e27c34db6fa55ba5e4636c8e19c5
arpeiks <57372063+arpeiks@users.noreply.github.com>
2023-02-15 09:06:59 +0100

    added POST to cors methods

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: "GET,HEAD,PUT,PATCH,DELETE,OPTIONS",
+    methods: "POST,GET,HEAD,PUT,PATCH,DELETE,OPTIONS",
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
