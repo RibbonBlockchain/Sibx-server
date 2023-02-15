9f2cc614db7bbe523f552ea40bad6fa37cb94d65
ayeolakenny <ayeolakenny@gmail.com>
2023-02-15 12:23:35 +0100

    creating bonds

--- dist/main.js
+++ dist/main.js
@@ -9,7 +9,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
         origin: true,
-        methods: "GET,HEAD,PUT,PATCH,DELETE,OPTIONS",
+        methods: "POST,GET,HEAD,PUT,PATCH,DELETE,OPTIONS",
         credentials: true,
     });
     app.useGlobalPipes(new common_1.ValidationPipe());
