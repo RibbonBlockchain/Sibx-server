85cb5666c33d66cd05c5ef500f1691056eed7abd
arpeiks <57372063+arpeiks@users.noreply.github.com>
2023-02-28 09:16:17 +0100

    Delete dist directory

--- dist/modules/cloudinary/cloudinary.service.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="multer" />
-import { UploadApiErrorResponse, UploadApiResponse } from "cloudinary";
-export declare class CloudinaryService {
-    uploadMedia(file: Express.Multer.File, folder: string): Promise<UploadApiResponse | UploadApiErrorResponse>;
-    deleteMedia(filename: any, resource_type: any): Promise<{
-        result: string;
-    }>;
-}
--- dist/modules/mail/mail.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { SendMailParams } from "./dto/mail.request";
-import { MailService } from "./mail.service";
-export declare class MailController {
-    private readonly mailService;
-    constructor(mailService: MailService);
-    sendMail(input: SendMailParams): Promise<void>;
-}
