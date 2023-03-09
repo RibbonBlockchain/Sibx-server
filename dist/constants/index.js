d43dc03d0001aa068a52ab331c3539dd4a83721e
ayeolakenny <ayeolakenny@gmail.com>
2023-03-09 11:41:56 +0100

    removed dist

--- dist/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IMAGE_TYPE = void 0;
-exports.IMAGE_TYPE = {
-    BOND: "bond",
-};
-//# sourceMappingURL=index.js.map
--- dist/modules/mail/mail.constants.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const MAIL_SUBJECT: {
-    VERIFY_ACCOUNT: string;
-};
-export declare const MAIL_MESSAGE: {
-    VERIFY_ACCOUNT: (token: string) => string;
-};
